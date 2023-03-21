a62d8afa3e03067a9a37cdba1b41ac5513b4309b
Mark Bastiaans <mbastiaans@schubergphilis.com>
2023-03-21 13:49:41 +0100

    Add gRPC OTel traces

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -16,12 +16,22 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/prometheus"
-	api "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	otel_prometheus_exporter "go.opentelemetry.io/otel/exporters/prometheus"
+	sdkmetric "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+
 	"go.opentelemetry.io/otel/metric/instrument"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/trace"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -29,19 +39,74 @@ import (
 const (
 	service string = "metrics_example"
 	team    string = "team_awesome"
+
+	grpcCollectorAddressEnvKey string = "OTEL_GRPC_COLLECTOR_ADDRESS"
 )
 
 func main() {
 	log.SetLevel(log.DebugLevel)
 	log.Info("Starting Golang Metrics Example Service")
 
-	// Set up the Prometheus exporter
-	exporter, err := prometheus.New()
+	// Set up termination logic
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := sync.WaitGroup{}
+	defer func() {
+		log.Debug("Waiting for goroutines to finish")
+		cancel()
+		wg.Wait()
+		log.Debug("Goroutines finished")
+	}()
+
+	// Set up the Prometheus metrics metricsExporter
+	metricsExporter, err := otel_prometheus_exporter.New()
 	if err != nil {
 		log.Fatal(err)
 	}
-	provider := metric.NewMeterProvider(metric.WithReader(exporter))
-	meter := provider.Meter(service)
+
+	// Set up the OpenTelemetry gRPC trace exporter
+	conn, err := grpc.DialContext(ctx, os.Getenv(grpcCollectorAddressEnvKey),
+		// Note the use of insecure transport here. TLS is recommended in production.
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to create gRPC connection to collector")
+	}
+
+	// Set up the OpenTelemetry metrics stack
+	meterProvider := metric.NewMeterProvider(metric.WithReader(metricsExporter))
+	meter := meterProvider.Meter(service)
+
+	// Set up the OpenTelemetry tracing stack
+	// Set up a trace exporter
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		log.WithError(err).Fatal("Failed to create trace exporter: %w", err)
+	}
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			// the service name used to display traces in backends
+			semconv.ServiceName(service),
+		),
+	)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to create resource")
+	}
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	// set global propagator to tracecontext (the default is no-op).
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	tracer := otel.Tracer(service)
 
 	// Start the Prometheus HTTP server
 	go serveMetrics()
@@ -52,18 +117,9 @@ func main() {
 		attribute.Key("team").String(team),
 	}
 
-	wg := sync.WaitGroup{}
-	ctx, cancel := context.WithCancel(context.Background())
-	defer func() {
-		log.Debug("Waiting for goroutines to finish")
-		cancel()
-		wg.Wait()
-		log.Debug("Goroutines finished")
-	}()
-
 	generateTrigonoMetrics(meter, attrs, &wg, ctx)
-	generateStandardHTTPMetrics(meter, attrs, &wg, ctx)
-	generateGorillaHTTPMetrics(meter, attrs, &wg, ctx)
+	generateStandardHTTPMetrics(tracer, attrs, &wg, ctx)
+	generateGorillaHTTPMetrics(tracer, attrs, &wg, ctx)
 
 	waitForInterrupt()
 }
@@ -77,7 +133,7 @@ func serveMetrics() {
 	}
 }
 
-func generateTrigonoMetrics(meter api.Meter, attrs []attribute.KeyValue, wg *sync.WaitGroup, ctx context.Context) {
+func generateTrigonoMetrics(meter sdkmetric.Meter, attrs []attribute.KeyValue, wg *sync.WaitGroup, ctx context.Context) {
 	interval := time.Second
 	log.Infof("Starting trigonometric metrics generation every %s", interval)
 
@@ -137,12 +193,12 @@ func generateTrigonoMetrics(meter api.Meter, attrs []attribute.KeyValue, wg *syn
 	}()
 }
 
-func observableGauge(meter api.Meter, name string, description string, attrs []attribute.KeyValue, valueFunc func() float64) error {
+func observableGauge(meter sdkmetric.Meter, name string, description string, attrs []attribute.KeyValue, valueFunc func() float64) error {
 	gauge, err := meter.Float64ObservableGauge(name, instrument.WithDescription(description))
 	if err != nil {
 		return err
 	}
-	_, err = meter.RegisterCallback(func(_ context.Context, o api.Observer) error {
+	_, err = meter.RegisterCallback(func(_ context.Context, o sdkmetric.Observer) error {
 		o.ObserveFloat64(gauge, valueFunc(), attrs...)
 		return nil
 	}, gauge)
@@ -164,8 +220,12 @@ func getRandomStatus() int {
 	return http.StatusOK
 }
 
-func getHTTPHandlerFunc(minDelay int, maxDelay int) http.HandlerFunc {
+func getHTTPHandlerFunc(tracer trace.Tracer, traceName string, minDelay int, maxDelay int) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		_, span := tracer.Start(ctx, traceName)
+		defer span.End()
+
 		sleep := rand.Intn(maxDelay-minDelay) + minDelay
 		status := getRandomStatus()
 		log.Tracef("HTTP request received, sleeping %dms and returning %d", sleep, status)
@@ -209,7 +269,7 @@ func doHTTPRequest(client *http.Client, uri *url.URL, pattern string) error {
 	return nil
 }
 
-func generateStandardHTTPMetrics(meter api.Meter, attrs []attribute.KeyValue, wg *sync.WaitGroup, ctx context.Context) {
+func generateStandardHTTPMetrics(tracer trace.Tracer, attrs []attribute.KeyValue, wg *sync.WaitGroup, ctx context.Context) {
 	interval := time.Millisecond * 500
 	log.Infof("Starting standard HTTP metrics generation every %s", interval)
 
@@ -217,20 +277,8 @@ func generateStandardHTTPMetrics(meter api.Meter, attrs []attribute.KeyValue, wg
 	min := 10
 	max := 100
 
-	// TODO: set up HTTP server metrics
-	// mw, _ := httpmetrics.NewMiddleware(cm, "ExampleStandardHTTPServerMetric")
-	// handler, err := httpmetrics.Handler(
-	// 	mw,
-	// 	getHTTPHandlerFunc(min, max),
-	// 	"/",
-	// )
-	// if err != nil {
-	// 	log.WithError(err).Fatal("Cannot initialize standard HTTP metrics handler")
-	// }
-
-	// server := httptest.NewServer(handler)
-
-	server := httptest.NewServer(getHTTPHandlerFunc(min, max))
+	// Set up HTTP server metrics
+	server := httptest.NewServer(getHTTPHandlerFunc(tracer, "http.server.net", min, max))
 
 	wg.Add(1)
 	go func() {
@@ -258,27 +306,18 @@ func generateStandardHTTPMetrics(meter api.Meter, attrs []attribute.KeyValue, wg
 	}()
 }
 
-func generateGorillaHTTPMetrics(meter api.Meter, attrs []attribute.KeyValue, wg *sync.WaitGroup, ctx context.Context) {
+func generateGorillaHTTPMetrics(tracer trace.Tracer, attrs []attribute.KeyValue, wg *sync.WaitGroup, ctx context.Context) {
 	interval := time.Millisecond * 500
 	log.Infof("Starting Gorilla HTTP metrics generation every %s", interval)
 
 	// Set up server
-	// Create our router with the metrics middleware
-	// TODO: add HTTP server metrics middleware
-	// mw, _ := httpmetrics.NewMiddleware(cm, "ExampleGorillaHTTPServerMetric")
 	r := mux.NewRouter()
-	// r.Use(httpmetrics.HandlerProvider(mw))
 
 	// Add paths
-	r.Methods("GET").Path("/").HandlerFunc(getHTTPHandlerFunc(10, 100))
-	r.Methods("GET").Path("/test1").HandlerFunc(getHTTPHandlerFunc(50, 150))
-	r.Methods("GET").Path("/test1/{resource1}").HandlerFunc(getHTTPHandlerFunc(20, 50))
-
-	// Add paths to middleware
-	// If the paths are not added, they are not monitored
-	// mw.ObservePath("/")
-	// mw.ObservePath("/test1")
-	// mw.ObservePath("/test1/{resource1}")
+	traceName := "http.server.mux"
+	r.Methods("GET").Path("/").HandlerFunc(getHTTPHandlerFunc(tracer, traceName, 10, 100))
+	r.Methods("GET").Path("/test1").HandlerFunc(getHTTPHandlerFunc(tracer, traceName, 50, 150))
+	r.Methods("GET").Path("/test1/{resource1}").HandlerFunc(getHTTPHandlerFunc(tracer, traceName, 20, 50))
 
 	// Get random port for server and serve
 	listener, err := net.Listen("tcp", ":0")
